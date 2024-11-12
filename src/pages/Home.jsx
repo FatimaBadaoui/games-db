7f42a5a3145eb34f5ed72358a902d519b90af946
Fatima Badaoui <fatimina615@gmail.com>
2024-11-12 20:02:59 +0100

    add pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,23 +7,28 @@ import GamesByGenre from "../components/GamesByGenre.jsx";
 const Home = () => {
   const [selectedGenre, setSelectedGenre] = useState("Action");
   const [gamesListByGenre, setGamesListByGenre] = useState([]);
+  const [genreId, setGenreId] = useState(4);
+  const [page, setPage] = useState(1);
 
-  const getGamesByGenresId = (genreId) => {
-    getGamesByGenre(genreId).then((response) => {
-      // console.log(response.data.results);
+  console.log(page);
+
+  const getGamesByGenresId = (genreId, pageNumber) => {
+    getGamesByGenre(genreId, pageNumber).then((response) => {
+      console.log(genreId, pageNumber);
+      setGenreId(genreId);
       setGamesListByGenre(response.data.results);
     });
   };
 
   useEffect(() => {
-    getGamesByGenresId(4);
+    getGamesByGenresId(4, 1);
   }, []);
 
   return (
     <div className="grid grid-cols-4 px-8">
       <div className="hidden md:block">
         <GenreList
-          getGamesByGenreId={(genreId) => getGamesByGenresId(genreId)}
+          getGamesByGenreId={(genreId) => getGamesByGenresId(genreId, 1)}
           selectedGenreName={(genreName) => setSelectedGenre(genreName)}
         />
       </div>
@@ -37,6 +42,31 @@ const Home = () => {
             <GamesByGenre gamesList={gamesListByGenre} />
           </>
         )}
+
+        <div className="w-full py-10 relative">
+          {page > 1 && (
+            <button
+              type="button"
+              className="w-[100px] bg-lightPink hover:bg-darkPink text-white font-bold py-2 px-4 rounded-xl absolute left-0"
+              onClick={() => {
+                setPage(page - 1);
+                getGamesByGenresId(genreId, page - 1);
+              }}
+            >
+              Previous
+            </button>
+          )}
+          <button
+            type="button"
+            className="w-[100px] bg-lightBlue hover:bg-darkBlue text-white font-bold py-2 px-4 rounded-xl absolute right-0"
+            onClick={() => {
+              setPage(page + 1);
+              getGamesByGenresId(genreId, page + 1);
+            }}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/services/globalApi.js
+++ src/services/globalApi.js
@@ -10,7 +10,7 @@ const getGenresList = axiosCreate.get(`/genres?key=${key}`);
 
 const getAllGames = axiosCreate.get(`/games?key=${key}`);
 
-const getGamesByGenre = (genreId) =>
-  axiosCreate.get(`/games?key=${key}&genres=${genreId}`);
+const getGamesByGenre = (genreId, page) =>
+  axiosCreate.get(`/games?key=${key}&genres=${genreId}&page=${page}`);
 
 export { getGenresList, getAllGames, getGamesByGenre };
